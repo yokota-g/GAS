2495ea8b32e716a1dd48ab5e2ed3fc1b4fad9d0c
shinichi yokota <49588152+yokota-g@users.noreply.github.com>
2021-06-23 14:40:56 +0900

    最初のコミットです。

---
+++ src/main/java/com/gmail/yokota101010/App.java
@@ -0,0 +1,13 @@
+package com.gmail.yokota101010;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/gmail/yokota101010/AppTest.java
@@ -0,0 +1,20 @@
+package com.gmail.yokota101010;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
