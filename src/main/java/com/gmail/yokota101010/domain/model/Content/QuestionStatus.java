2b0a3fbb852f2363a368832903fd3d078533560a
shinichi yokota <49588152+yokota-g@users.noreply.github.com>
2021-06-25 07:15:38 +0900

    fix some more tiny thing

---
+++ src/main/java/com/gmail/yokota101010/domain/model/Content/QuestionStatus.java
@@ -0,0 +1,6 @@
+package com.gmail.yokota101010.domain.model.Content;
+
+public enum QuestionStatus {
+    Open,
+    Close;
+}
---
+++ src/main/java/com/gmail/yokota101010/port/adapter/persistence/Content/HashMapContentRepository.java
@@ -0,0 +1,30 @@
+package com.gmail.yokota101010.port.adapter.persistence.Content;
+
+import java.util.HashMap;
+
+import com.gmail.yokota101010.domain.model.Content.Content;
+import com.gmail.yokota101010.domain.model.Content.ContentRepository;
+
+public class HashMapContentRepository extends ContentRepository {
+    public HashMap<String,Content> contents = new HashMap<>();
+
+    @Override
+    public long count(){
+        return contents.size();
+    }
+
+    @Override
+    public void save(String id,Content content) {
+        contents.put(content.id,content);
+    }
+
+    @Override
+    public Content contentNamed(String name) {
+        for(Content content : contents.values()){
+            if(content.name.equals(name)){
+                return content;
+            }
+        }
+        return null;
+    }
+}
